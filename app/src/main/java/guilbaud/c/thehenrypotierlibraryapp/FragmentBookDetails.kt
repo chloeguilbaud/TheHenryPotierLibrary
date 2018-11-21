d01b497e3dd65b290dc0cd0fd6f5115fd07538a8
Chloe <44chloeguilbaud@gmail.com>
2018-11-21 16:10:41 +0100

    feat: Project frames initialisations

---
+++ app/src/main/java/guilbaud/c/thehenrypotierlibraryapp/FragmentBookDetails.kt
@@ -0,0 +1,8 @@
+package guilbaud.c.thehenrypotierlibraryapp
+
+import android.support.v4.app.Fragment
+
+class FragmentBookDetails : Fragment() {
+
+
+}
---
+++ app/src/main/java/guilbaud/c/thehenrypotierlibraryapp/FragmentBookList.kt
@@ -0,0 +1,15 @@
+package guilbaud.c.thehenrypotierlibraryapp
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class FragmentBookList : Fragment() {
+
+
+}
