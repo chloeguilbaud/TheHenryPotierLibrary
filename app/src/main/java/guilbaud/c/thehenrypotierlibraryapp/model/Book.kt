8ab02aa130141d02f769b0979fc3e4dd33c3311a
Chloe <44chloeguilbaud@gmail.com>
2018-11-22 14:24:03 +0100

    feat: Récupération des résumés de livre

--- app/src/main/java/guilbaud/c/thehenrypotierlibraryapp/model/Book.kt
+++ app/src/main/java/guilbaud/c/thehenrypotierlibraryapp/model/Book.kt
@@ -4,4 +4,4 @@ package guilbaud.c.thehenrypotierlibraryapp.model
  * Book data model representation.
  * @author Chloe GUILBAUD
  */
-data class Book(val isbn: String, val title: String, val price: String, val cover: String)
+data class Book(val isbn: String, val title: String, val price: String, val cover: String, val synopsis: Array<String>)
