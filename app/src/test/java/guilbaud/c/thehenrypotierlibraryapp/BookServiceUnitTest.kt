7063026cc69e855334be0ac1ba0273f63779d3b8
Chloe <44chloeguilbaud@gmail.com>
2018-11-27 16:59:41 +0100

    test: Service test

---
+++ app/src/test/java/guilbaud/c/thehenrypotierlibraryapp/BookServiceUnitTest.kt
@@ -0,0 +1,26 @@
+package guilbaud.c.thehenrypotierlibraryapp
+
+import guilbaud.c.thehenrypotierlibraryapp.model.Book
+import guilbaud.c.thehenrypotierlibraryapp.service.BookService
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BookServiceUnitTest {
+
+    /*@Test
+    fun book_fetching() {
+        BookService.fetchBooks { bookList : Array<Book> ->
+            checkBookList(bookList)
+        }
+    }
+
+    private fun checkBookList(bookList: Array<Book>) {
+        assertEquals("Book list size", 7, bookList.size)
+        assertEquals("Book content title", "Henri Potier à l'école des sorciers", bookList[0].title)
+        assertEquals("Book content price", "35", bookList[0].price)
+        assertEquals("Book content cover", "35", bookList[0].cover)
+        assertEquals("Book content synopsis", 3, bookList[0].synopsis.size)
+    }*/
+
+}
